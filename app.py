cc8b68202a13592cc152fae426c49fe22a1f7507
Fred Rybin <fredrybin@gmail.com>
2018-04-25 21:04:51 -0400

    Trying to get things to work correctly on openshift with https

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from smartskill import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host=app.config['IP'], port=int(app.config['PORT']), debug=True)
     
--- smartskill/__init__.py
+++ smartskill/__init__.py
@@ -1,9 +1,19 @@
 import logging
 import requests
+import os
 from flask import Flask, render_template
 from flask_ask import Ask, statement
 
 app = Flask(__name__)
+
+
+# Get app config from absolute file path
+if os.path.exists(os.path.join(os.getcwd(), "config.py")):
+    app.config.from_pyfile(os.path.join(os.getcwd(), "config.py"))
+else:
+    app.config.from_pyfile(os.path.join(os.getcwd(), "config.env.py"))
+
+
 ask = Ask(app, "/")
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 URL = "https://smartroom-api.csh.rit.edu/"
