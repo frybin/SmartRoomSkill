9554d71ba62ab43471b5f0b5d5791430f6a9b861
Fred Rybin <fredrybin@gmail.com>
2018-04-25 22:11:29 -0400

    made the correct servername

--- config.env.py
+++ config.env.py
@@ -4,7 +4,7 @@ from os import environ as env
 # Flask config
 IP = env.get('IP', '0.0.0.0')
 PORT = env.get('PORT', 8080)
-SERVER_NAME = env.get('SERVER_NAME', 'alexa-smart-endpoint-alexaroom.a.csh.rit.edu')
+SERVER_NAME = env.get('SERVER_NAME', 'smartroom-alexa.csh.rit.edu')
 
 # Openshift secret
 SECRET_KEY = env.get("SECRET_KEY", default=''.join(secrets.token_hex(16)))
