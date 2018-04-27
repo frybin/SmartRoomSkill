aa77fd938d976dad3a5dad809922131c7dba7cd3
Fred Rybin <fredrybin@gmail.com>
2018-04-27 15:59:02 -0400

    Updates utterances and added a few more intents

--- smartskill/__init__.py
+++ smartskill/__init__.py
@@ -22,7 +22,8 @@ URL = "https://smartroom-api.csh.rit.edu/"
 @ask.launch
 def launch():
     welcome_msg = render_template('welcomeStatment')
-    return question(welcome_msg)
+    return question(welcome_msg).simple_card(
+        title='Welcome to CSH SmartRoom', content='say Shades to interact with the windows')
 
 @ask.intent("ShadesIntent")
 def shades_intent():
@@ -30,7 +31,7 @@ def shades_intent():
     request = requests.get(url=shades_url)
     data = request.json()
     msg = render_template('shadeStatus', percent=data['howOpen'])
-    return question(msg)
+    return question(msg).simple_card(title='Window Blinds control', content='say Shades to interact with the windows')
 
 @ask.intent("ChangeShadesIntent", convert={'percent': int})
 def change_shades_intent(percent):
@@ -41,4 +42,43 @@ def change_shades_intent(percent):
         msg = render_template('ChangeShadesIntent', percent=percent)
     else:
         msg = render_template('failed')
+    return statement(msg).simple_card(
+        title='Welcome to CSH SmartRoom', content='say Shades to interact with the windows')
+
+@ask.intent("CloseShadesIntent")
+def close_shades_intent():
+    shades_url = URL+"shades"
+    data = {'howOpen': 100}
+    request = requests.post(url=shades_url, json=data, verify=True)
+    if request.text == "Success":
+        msg = render_template('CloseShadesIntent')
+    else:
+        msg = render_template('failed')
     return statement(msg)
+
+@ask.intent("OpenShadesIntent")
+def open_shades_intent():
+    shades_url = URL+"shades"
+    data = {'howOpen': 0}
+    request = requests.post(url=shades_url, json=data, verify=True)
+    if request.text == "Success":
+        msg = render_template('OpenShadesIntent')
+    else:
+        msg = render_template('failed')
+    return statement(msg)
+
+@ask.intent("AMAZON.HelpIntent")
+def help_intent():
+    msg = render_template('help')
+    return statement(msg).simple_card(title='Smart Room Help Commands', content=msg)
+
+@ask.intent("ShadeStatusHelp")
+def status_help_intent():
+    msg = render_template('ShadeStatusHelp')
+    return statement(msg).simple_card(title='Smart Room Help Commands', content=msg)
+
+@ask.intent("ChangeShadesHelp")
+def change_help_intent():
+    msg = render_template('ChangeShadesHelp')
+    return statement(msg).simple_card(title='Smart Room Help Commands', content=msg)
+    
